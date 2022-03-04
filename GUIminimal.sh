8a25610a0a8c53e89c8f77cdd6f31c63bb5d17d2
anshikthind <anshikthind@gmail.com>
2022-03-04 20:41:53 +0530

    date 3/4/2022

---
+++ GUIminimal.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+R="$(printf '\033[1;31m')"
+G="$(printf '\033[1;32m')"
+Y="$(printf '\033[1;33m')"
+W="$(printf '\033[1;37m')"
+C="$(printf '\033[1;36m')"
+
+apt update -y && 
+    apt install sudo -y && 
+    apt install wget apt-utils locales-all dialog -y
+banner() {
+    clear
+
+    printf "\033[0m\n"
+    printf "     \033[32mA modded gui version of ubuntu for Termux\033[0m\n"
+    printf "\033[0m\n"
+
+}
+
+package() {
+    banner
+    echo -e "${R} [${W}-${R}]${C} Checkinggg required packages..."${W}
+    sudo apt-get update -y
+    sudo apt install udisks2 -y
+    sudo rm /var/lib/dpkg/info/udisks2.postinst
+    echo "" > /var/lib/dpkg/info/udisks2.postinst
+    sudo dpkg --configure -a
+    sudo apt-mark hold udisks2
+    packs=(sudo wget curl nano git keyboard-configuration tzdata xfce4 xfce4-goodies xfce4-terminal librsvg2-common firefox menu inetutils-tools dialog exo-utils tigervnc-standalone-server tigervnc-common dbus-x11 fonts-beng fonts-beng-extra vlc gtk2-engines-murrine gtk2-engines-pixbuf)
+    for hulu in "${packs[@]}"; do
+        type -p "$hulu" &>/dev/null || {
+            echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$hulu${C}"${W}
+            sudo apt-get install "$hulu" -y --no-install-recommends
+        }
+    done
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get clean
+}
+
+
+
+theme() {
+    theme=(Bright Xfce-flat Daloa Xfce-kde2 Xfce-kolors Xfce-4.4 Xfce-light Xfce-4.6 Xfce-orange Emacs Xfce-b5 Xfce-redmondxp Xfce-basic Xfce-saltlake Moheli Xfce-cadmium Xfce-smooth Xfce-curve Xfce-stellar Retro Xfce-dawn Xfce-winter Smoke Xfce-dusk)
+    for rmi in "${theme[@]}"; do
+        type -p "$rmi" &>/dev/null || {
+            sudo rm -rf /usr/share/themes/"$rmi"
+        }
+    done
+}
+
+font() {
+    fonts=(hicolor LoginIcons ubuntu-mono-light)
+    for rmf in "${fonts[@]}"; do
+        type -p "$rmf" &>/dev/null || {
+            sudo rm -rf /usr/share/icons/"$rmf"
+        }
+    done
+}
+
+refs() {
+    sudo apt-get update -y
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+    sudo apt-get upgrade -y
+    sudo apt autoremove -y
+    banner
+    echo -e "${R} [${W}-${R}]${C} Installing Visual Studio..."${W}
+
+#    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+ #   sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+ #   sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt install apt-transport-https -y
+   sudo apt update -y
+  #  sudo apt install code -y
+
+    git clone --depth=1 https://github.com/vinceliuice/Layan-gtk-theme.git $HOME/Layan-gtk-theme
+    sudo chmod +x $HOME/Layan-gtk-theme/install.sh
+    sudo bash $HOME/Layan-gtk-theme/install.sh
+
+    git clone --depth=1 https://github.com/vinceliuice/Qogir-icon-theme.git $HOME/Qogir-icon-theme
+    sudo chmod +x $HOME/Qogir-icon-theme/install.sh
+    sudo bash $HOME/Qogir-icon-theme/install.sh
+
+    git clone --depth=1 https://github.com/s-h-3-l-l/katoolin3.git $HOME/katoolin3
+    sudo chmod +x $HOME/katoolin3/install.sh
+    cd $HOME/katoolin3 && sudo bash install.sh
+
+    sudo apt update -y
+    sudo apt autoremove -y
+
+}
+
+vnc() {
+    banner
+    echo -e "${R} [${W}-${R}]${C} Setting up VNC Server..."${W}
+
+    if [[ ! -d "$HOME/.vnc" ]]; then
+        mkdir -p "$HOME/.vnc"
+    fi
+
+    if [[ -e "$HOME/.vnc/xstartup" ]]; then
+        rm -rf $HOME/.vnc/xstartup
+    fi
+
+    wget https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/xstartup
+    mv -f xstartup $HOME/.vnc/xstartup
+    chmod +x $HOME/.vnc/xstartup
+
+    if [[ -e "/usr/local/bin/vncstart" ]]; then
+        rm -rf /usr/local/bin/vncstart
+    fi
+
+    wget https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/vncstart
+    mv -f vncstart /usr/local/bin/vncstart
+    chmod +x /usr/local/bin/vncstart
+
+    if [[ -e "/usr/local/bin/vncstop" ]]; then
+        rm -rf /usr/local/bin/vncstop
+    fi
+
+    wget https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/vncstop
+    mv -f vncstop /usr/local/bin/vncstop
+    chmod +x /usr/local/bin/vncstop
+
+    echo "export DISPLAY=":1"" >> /etc/profile
+    echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile 
+    source /etc/profile
+
+}
+
+note() {
+    banner
+    echo -e " ${G} Successfully Installed !"${W}
+    sleep 1
+    echo
+    echo -e " ${G}Type ${C}vncstart${G} to run Vncserver."${W}
+    echo -e " ${G}Type ${C}vncstop${G} to stop Vncserver."${W}
+    echo
+    echo -e " ${C}Install VNC VIEWER Apk on your Device."${W}
+    echo
+    echo -e " ${C}Open VNC VIEWER & Click on + Button."${W}
+    echo
+    echo -e " ${C}Enter the Address localhost:1 & Name anything you like."${W}
+    echo
+    echo -e " ${C}Set the Picture Quality to High for better Quality."${W}
+    echo
+    echo -e " ${C}Click on Connect & Input the Password."${W}
+    echo
+    echo -e " ${C}Enjoy :D"${W}
+    echo
+    echo
+
+}
+
+package
+chromium
+theme
+font
+refs
+vnc
+note
